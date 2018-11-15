3ac23632c9aa620505bb5ecfdea7061e1ddc4c99
Hrafnkell Baldursson <kotkarl@gmail.com>
2018-11-15 22:25:25 +0000

    source code added

---
+++ src/components/CssModuleComponent/CssModuleComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './CssModuleComponent.module.scss'
+
+const CssModuleComponent = () => (
+    <div className={ styles.container }>
+        <h2>CssModuleComponent</h2>
+    </div>
+)
+
+export default CssModuleComponent
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import CssModuleComponent from '../CssModuleComponent'
+
+const Header = () => (
+    <header>
+        <h1>Header</h1>
+    </header>
+)
+
+export default Header

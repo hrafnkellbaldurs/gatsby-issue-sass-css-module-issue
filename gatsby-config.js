e319cb4532e17f6c74d47b7e987183491b30a0a1
Hrafnkell Baldursson <kotkarl@gmail.com>
2018-11-16 15:35:03 +0000

    Added StaticQuery to Layout. Still works as expected

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
-const { name: repoName } = require('./package.json')
+const {
+    name,
+    repository: { url: repoUrl },
+    description,
+    author,
+    keywords
+} = require('./package.json')
+const repoName = repoUrl.split('/')[repoUrl.length - 1]
 
 module.exports = {
     pathPrefix: `/${repoName}`,
+    siteMetadata: {
+        title: name,
+        description,
+        author,
+        keywords: keywords.join(', ')
+    },
     plugins: [
         `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-sass`,
+        `gatsby-plugin-sass`
     ]
 }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 import Header from '../Header'
 
 const Layout = ({ children }) => (
-    <>
-        <Helmet
-            title="HrafnkellBaldurs"
-            meta={[
-                { name: 'description', content: 'meta description' },
-                { name: 'author', content: 'Hrafnkell Baldursson' },
-                { name: 'keywords', content: 'meta keywords' },
-            ]}
-        >
-            <html lang="en"/>
-        </Helmet>
-        <Header/>
-        <>
-            { children }
-        </>
-    </>
+    <StaticQuery
+        query={ graphql`
+            query SiteMetaDataQuery {
+                site {
+                    siteMetadata {
+                        title
+                        description
+                        author
+                    }
+                }
+            }
+        `}
+        render={ data => (
+            <>
+                <Helmet
+                    title={ data.site.siteMetadata.title }
+                    meta={[
+                        { name: 'description', content: data.site.siteMetadata.description },
+                        { name: 'author', content: data.site.siteMetadata.author },
+                        { name: 'keywords', content: 'sample, something' }
+                    ]}
+                >
+                    <html lang="en"/>
+                </Helmet>
+                <Header/>
+                <>
+                    { children }
+                </>
+            </>
+        ) }
+    />
+
+    
 )
 
 Layout.propTypes = {
